c2fa809d580f35c55528efe25e89673ca0243cc0
unknown <utsavsuthar24@gmail.com>
2022-07-16 00:25:51 +0530

    calculate unknown amount of numbers

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ module.exports = class StringCalculator{
         if(numberString == ''){
             return 0;
         }
+        else if(numberString.length === 1){
+            return (parseInt(numberString));
+        }
+        else{
+            var res = numberString.split(',');
+            var total=0;
+
+            for( var i=0;i<res.length;i++){
+                total+= parseInt(res[i]);
+            }
+            return total;
+        }
+
     }
 };
--- index.spec.js
+++ index.spec.js
@@ -10,4 +10,25 @@ describe('Test',()=>{
     it('Calculate empty string',()=>{
         assert.strictEqual(this.calculator.calculate(''),0);
     })
+
+    it('Calculate simple string with one element',()=>{
+        assert.strictEqual(this.calculator.calculate('1'),1);
+    })
+
+    it('Calculate simple string with one element',()=>{
+        assert.strictEqual(this.calculator.calculate('9'),9);
+    })
+    
+    it('Calculate simple string with two element',()=>{
+        assert.strictEqual(this.calculator.calculate('1,2'),3);
+    })
+
+    it('Calculate simple string with two element',()=>{
+        assert.strictEqual(this.calculator.calculate('1,33'),34);
+    })
+
+    it('Calculate simple string with three element',()=>{
+        assert.strictEqual(this.calculator.calculate('1,2,4'),7);
+    })
+
 })
